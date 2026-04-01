7a8ee3facf4517692a30e2d1404c78cf738998ad
Liu Liu <i@liuliu.me>
2026-04-01 00:57:35 -0400

    Update docc.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "media-generation-kit",
+  name: "MediaGenerationKit",
   platforms: [.macOS(.v13), .iOS(.v16)],
   products: [
     .library(name: "MediaGenerationKit", targets: ["MediaGenerationKit"]),
@@ -12,6 +12,7 @@ let package = Package(
     // Use a tagged upstream release before publishing this wrapper package.
     .package(url: "https://github.com/drawthingsai/draw-things-community.git", branch: "main"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.1"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.5"),
   ],
   targets: [
     .target(
